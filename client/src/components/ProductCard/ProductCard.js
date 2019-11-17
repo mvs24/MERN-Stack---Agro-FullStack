58f3f5c6b2793266791f07b6b412a4e30ca51e29
Marius <Marius.Vasili@fti.edu.al>
2019-11-17 11:43:24 +0100

    Product Card frontend

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
-import Moment from 'react-moment';
+import Moment from "react-moment";
+import { connect } from "react-redux";
 
 import "./ProductCard.css";
 
 class ProductCard extends Component {
+  state = {
+    quantity: "",
+    shouldChange: true
+  };
+
+  addToCart = product => {
+    let dataToBuy = {
+      price: this.state.quantity * 1 * product.medPrice * 1,
+      date: Date.now,
+      nameOfProduct: product.name,
+      productId: product._id
+    };
+    // this.props.addProductToCart
+  };
+
   render() {
     const product = this.props.product;
-    console.log(product);
+    const userId = this.props.user.user._id;
 
     const randomNumber = () => {
-      return Math.floor(Math.random() * 3);
+      if (this.state.shouldChange) {
+        return Math.floor(Math.random() * 3);
+      }
     };
 
     let image =
@@ -24,8 +42,9 @@ class ProductCard extends Component {
 
     return (
       <div className="col-1-of-3">
-        <div className="card">
-          <div className="card__side card__side__front">
+        <div className= {product.user.toString() === userId.toString() ? "cardOwner" : "card"}>
+          {product.user.toString() === userId.toString() ? (
+            <div className="card__side card__side__front">
             <div
               className="card__picture"
               style={{
@@ -51,18 +70,89 @@ class ProductCard extends Component {
             <div className="card__detail">
               <ul>
                 <li>Small Price: {product.smallPrice}</li>
-                <li >Big Price: {product.bigPrice}</li>
+                <li>Big Price: {product.bigPrice}</li>
                 <li>Medium Price: {product.medPrice}</li>
                 <li>Quantity: {product.quantity}</li>
-                <li>Date: <Moment format='dd-mm-yyyy'> {product.date}</Moment></li>
+                <li>
+                  Date:{" "}
+                  <Moment format="DD:MM:YYYY  HH:mm">{product.date}</Moment>
+                </li>
               </ul>
             </div>
           </div>
-          <div className={classNames}>back</div>
+          ) : (
+              <div className="card__side card__side__front">
+              <div
+                className="card__picture"
+                style={{
+                  backgroundImage: `url(${image})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  height: "14.375rem"
+                }}
+              >
+                &nbsp;
+              </div>
+              <div className="card__heading">
+                <span
+                  style={{
+                    fontSize: "1.1rem",
+                    color: "rgb(197, 187, 187)"
+                  }}
+                >
+                  Name:
+                </span>
+                {product.name}
+              </div>
+              <div className="card__detail">
+                <ul>
+                  <li>Small Price: {product.smallPrice}</li>
+                  <li>Big Price: {product.bigPrice}</li>
+                  <li>Medium Price: {product.medPrice}</li>
+                  <li>Quantity: {product.quantity}</li>
+                  <li>
+                    Date:{" "}
+                    <Moment format="DD:MM:YYYY  HH:mm">{product.date}</Moment>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
+        
+          {product.user.toString() === userId.toString() ? null : (
+            <div className={classNames}>
+              <input
+                onChange={e =>
+                  this.setState({
+                    quantity: e.target.value,
+                    shouldChange: false
+                  })
+                }
+                type="number"
+                className="quantityInput"
+                value={this.state.quantity}
+                placeholder="Quantity to buy"
+              />
+              {/* <input type="text" value={}/> */}
+              <button
+                disabled={this.state.quantity <= 0}
+                className={
+                  this.state.quantity <= 0 ? "btnPos" : "pointer btnPos"
+                }
+                onClick={() => this.addToCart(product)}
+              >
+                Add to cart
+              </button>
+            </div>
+          )}
         </div>
       </div>
     );
   }
 }
 
-export default ProductCard;
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+export default connect(mapStateToProps)(ProductCard);
