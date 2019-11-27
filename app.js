5758245d92082be3fcb9ebdfe4ebd328c99198a8
Marius <Marius.Vasili@fti.edu.al>
2019-11-27 20:41:17 +0100

    Prepared for deploy

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const formidable = require('express-formidable');
 const cloudinary = require('cloudinary');
+const path = require('path');
 
 const app = express();
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";    
@@ -62,6 +63,13 @@ app.get(`/api/removeFile`, auth, protect('seller'), (req, res) => {
   })
 })   
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const port = 5000;
 app.listen(port, () => console.log("App listening on port " + port));
