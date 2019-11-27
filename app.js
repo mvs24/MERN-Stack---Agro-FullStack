124f3de57c94c3f9b9b9c27c060b19750a3126e3
Marius <Marius.Vasili@fti.edu.al>
2019-11-27 22:22:33 +0100

    Prepared for deploy

--- app.js
+++ app.js
@@ -80,6 +80,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
- 
-const port = 5000 || process.env.PORT;
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("App listening on port " + port));
