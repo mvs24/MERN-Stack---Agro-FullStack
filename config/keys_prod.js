3c182b4039aad6416139d8355021a6277513d962
Marius <Marius.Vasili@fti.edu.al>
2019-11-27 21:32:33 +0100

    Prepared for deploy

---
+++ config/keys_prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+  superSecret: process.env.SUPER_SECRET
+};
